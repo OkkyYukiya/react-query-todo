6e04a6aa3928f8e72d078085a39f556c196933c4
YUKIYA OKITA <59529919+OkkyYukiya@users.noreply.github.com>
2021-06-04 18:33:59 +0900

    first commit

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+export interface Task {
+  id: number
+  title: string
+  tag: number
+  tag_name: string
+  created_at: string
+  updated_at: string
+}
+
+export interface EditTask {
+  id: number
+  title: string
+  tag: number
+}
